2cfb486feac8729e91ade1dd99ce2f9007a9cd51
BohdanRadchenko <radchenkobs@gmail.com>
2019-07-18 19:35:24 +0300

    Updates

---
+++ precache-manifest.afd7a32cf0d5b551babc0470d4709784.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913155980de29dd9765f97cd80b9ed98",
+    "url": "/goit-react-hw-02-movies/index.html"
+  },
+  {
+    "revision": "9c956d3b048b30f2b42c",
+    "url": "/goit-react-hw-02-movies/static/css/main.309d6635.chunk.css"
+  },
+  {
+    "revision": "0a052bb7d76e5c6c8a69",
+    "url": "/goit-react-hw-02-movies/static/js/2.5cd37059.chunk.js"
+  },
+  {
+    "revision": "9c956d3b048b30f2b42c",
+    "url": "/goit-react-hw-02-movies/static/js/main.246f9c03.chunk.js"
+  },
+  {
+    "revision": "c0f0bf3555f23a72488d",
+    "url": "/goit-react-hw-02-movies/static/js/runtime~main.4f93631a.js"
+  }
+]);
